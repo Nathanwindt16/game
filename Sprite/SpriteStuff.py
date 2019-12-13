d5c64caf82b26b2515a8d238c73e3b87aa628da7
atinder.singh <atinder.singh@surreyschools.ca>
2019-12-13 10:03:41 -0800

    testing the commit and push

--- Sprite/SpriteStuff.py
+++ Sprite/SpriteStuff.py
@@ -3,6 +3,7 @@ SIZE = WIDTH, HEIGHT = 1925, 1000  # the width and height of our screen
 BACKGROUND_COLOR = pygame.Color('white')  # The background colod of our window
 FPS = 100  # Frames per second
 
+#comment
 
 class MySprite(pygame.sprite.Sprite):
     def __init__(self):
