016256d0f5612b7522f8db41fc666768f793aeb3
atinder.singh <atinder.singh@surreyschools.ca>
2020-01-14 09:39:09 -0800

    i did majore work on the car game

--- Car Game.py
+++ Car Game.py
@@ -2,6 +2,7 @@
 
 import pygame
 import time
+import sys
 import random
 pygame.init()
 #color
@@ -16,7 +17,41 @@ bright_green=(0,255,0)
 window_width=800
 window_hight=600
 #display surface
-gd=pygame.display.set.mode((window_width, window_hight))
-carimg =pygame.image.load('./Car Images/Police.png')
+gd = pygame.display.set_mode([window_width, window_width])
+carimg =pygame.image.load('Car_Images/Topdown_vehicle_sprites_pack/Car.png')
 car1=pygame.transform.scale(carimg, (100,100))
 clock = pygame.time.Clock()
+
+def car (x,y) :
+    gd.blit(car1, (x, y))
+    pygame.display.update()
+
+def gameloop() :
+    x_change = 0
+    y_lead = 0
+    x = 300
+    y = 490
+    block = 10
+    game_over = False
+    while game_over == False:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over= True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -block
+                elif event.key == pygame.K_RIGHT:
+                    x_change = +block
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x += x_change
+        gd.fill(black)
+        car(x,y)
+
+        clock.tick(60)
+        pygame.display.update()
+
+
+gameloop()
